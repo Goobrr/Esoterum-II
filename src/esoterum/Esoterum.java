aac4d818ae6967024e954e724073adfa6f198855
Eldoofus <49511921+Eldoofus@users.noreply.github.com>
2024-10-12 21:10:40 +0800

    ok now its correct

--- src/esoterum/Esoterum.java
+++ src/esoterum/Esoterum.java
@@ -119,7 +119,7 @@ public class Esoterum extends Mod
 
         new SignalBlock("display")
         {{
-            vertexCount = 1;
+            vertexCount = 2;
             hasGraph = false;
             setConns(0, 0, 0, 0);
             setInputs(0, 0, 1, 0);
